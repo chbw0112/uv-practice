5492cb20519aa2a0b8eb0e3c5532510f4801397a
clhw0112 <chbw0112@gmail.com>
2026-05-22 07:52:57 +0900

    os_practice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import requests
-response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-print(f"상태 코드: {response.status_code}")
-data = response.json()
-print(f"할 일 제목: {data['title']}")
-print(f"완료 여부: {data['completed']}")
-if response.status_code == 200:
-    print("✅ 성공!")
-else:
-    print("❌ 뭔가 잘못됐어요")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+pas = os.getenv("password")
+print(pas)
